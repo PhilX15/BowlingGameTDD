fb52dfe77bc1ccbfbb266ec15ac320329004eb59
PhilX15 <filipkal55@gmail.com>
2025-06-18 13:21:40 +0200

    Make strike in the last frame test pass

--- src/tdd/Game.java
+++ src/tdd/Game.java
@@ -1,19 +1,22 @@
 package tdd;
 
 public class Game {
-	
+
 	private int[] rolls = new int[21];
 	private int currentRoll;
 
 	public void roll(int pins) {
 		rolls[currentRoll++] = pins;
 	}
-	
+
 	public int score() {
 		int score = 0;
 		int rollCounter = 0;
 		for (int frame = 0; frame < 10; frame++) {
-			if (isSpare(rollCounter)) {
+			if (rolls[rollCounter] == 10) {
+				score += 10 + rolls[rollCounter + 1] + rolls[rollCounter + 2];
+				rollCounter++;
+			} else if (isSpare(rollCounter)) {
 				score += spareBonus(rollCounter);
 				rollCounter += 2;
 			} else {
@@ -21,7 +24,7 @@ public class Game {
 				rollCounter += 2;
 			}
 		}
-		
+
 		return score;
 	}
 
