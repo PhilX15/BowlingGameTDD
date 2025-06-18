957594c5a83d14f1ae97e0118d00ac78a9059b75
PhilX15 <filipkal55@gmail.com>
2025-06-18 12:26:14 +0200

    Make spare in the last frame test pass

--- src/tdd/Game.java
+++ src/tdd/Game.java
@@ -1,12 +1,24 @@
 package tdd;
 
 public class Game {
+	
+	private int[] rolls = new int[21];
+	private int currentRoll;
 
 	public void roll(int pins) {
-		return;
+		rolls[currentRoll++] = pins;
 	}
 	
 	public int score() {
-		return 0;
+		int score = 0;
+		for (int frame = 0; frame < 10; frame++) {
+			if (rolls[frame * 2] + rolls[frame * 2 + 1] == 10) {
+				score += 10 + rolls[(frame + 1) * 2];
+			} else {
+				score += rolls[frame * 2] + rolls[frame * 2 + 1];
+			}
+		}
+		
+		return score;
 	}
 }
