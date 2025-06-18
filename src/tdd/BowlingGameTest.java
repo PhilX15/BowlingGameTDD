b5dc0d9a86013825208bbbd6038c42749b8f80ca
PhilX15 <filipkal55@gmail.com>
2025-06-18 13:17:24 +0200

    Add failing test for strike in the last frame

--- src/tdd/BowlingGameTest.java
+++ src/tdd/BowlingGameTest.java
@@ -6,32 +6,43 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class BowlingGameTest {
-	
-	private Game g;
-	
-	@BeforeEach
-	void setup() {
-		g = new Game();
-	}
-	
-	@Test
-	void testSpareInTheLastFrame() {
-		rollMany(18, 0);
-		
-		rollSpare();
-		g.roll(3);
-		
-		assertEquals(13, g.score());
-	}
+    
+    private Game g;
+    
+    @BeforeEach
+    void setup() {
+        g = new Game();
+    }
+    
+    @Test
+    void testSpareInTheLastFrame() {
+        rollMany(18, 0);
+        
+        rollSpare();
+        g.roll(3);
+        
+        assertEquals(13, g.score());
+    }
+    
+    @Test
+    void testStrikeInTheLastFrame() {
+        rollMany(18, 0);
+        
+        g.roll(10);
+        g.roll(3);
+        g.roll(5);
+        
+        assertEquals(18, g.score());
+    }
 
-	private void rollSpare() {
-		g.roll(5);
-		g.roll(5);
-	}
+    private void rollSpare() {
+        g.roll(5);
+        g.roll(5);
+    }
 
-	private void rollMany(int rolls, int pins) {
-		for (int i = 0; i < rolls; i++) {
-			g.roll(pins);
-		}
-	}
+    private void rollMany(int rolls, int pins) {
+        for (int i = 0; i < rolls; i++) {
+            g.roll(pins);
+        }
+    }
 }
